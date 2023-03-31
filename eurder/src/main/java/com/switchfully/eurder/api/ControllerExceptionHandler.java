1e1a9dcc5fa83f9d2c79c537ad15e5abc5c31ccb
TournayGilles <Tournay.gilles.94@gmail.com>
2023-03-31 15:42:42 +0200

    Refactoring Exception Handling

--- eurder/src/main/java/com/switchfully/eurder/api/ControllerExceptionHandler.java
+++ eurder/src/main/java/com/switchfully/eurder/api/ControllerExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.switchfully.eurder.api;
 
+import com.switchfully.eurder.internals.exceptions.NoCustomerWithProvidedIdException;
 import com.switchfully.eurder.internals.exceptions.NoRightException;
+import com.switchfully.eurder.internals.exceptions.NoSuchItemFoundException;
+import com.switchfully.eurder.internals.exceptions.NoSuchOrderException;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,17 +16,21 @@ import java.io.IOException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
     @ExceptionHandler(NoRightException.class)
     protected void IllegalAccess(HttpServletResponse response) throws IOException {
         response.sendError(HttpStatus.UNAUTHORIZED.value(), "You don't have the right to do this");
     }
-    @ExceptionHandler(IllegalArgumentException.class)
-    protected void IllegalArgumentHandler(HttpServletResponse response) throws IOException{
+    @ExceptionHandler({IllegalArgumentException.class, NoCustomerWithProvidedIdException.class, NoSuchOrderException.class, NoSuchItemFoundException.class})
+    protected void IllegalArgumentHandler(Exception exception, HttpServletResponse response) throws IOException{
+        logger.error(exception.getMessage(),exception);
         response.sendError(HttpStatus.BAD_REQUEST.value(), "Something went wrong with the Id provided");
 
     }
     @ExceptionHandler(RuntimeException.class)
-    protected void generalException(HttpServletResponse response) throws IOException{
+    protected void generalException(Exception exception, HttpServletResponse response) throws IOException{
+        logger.error(exception.getMessage(),exception);
         response.sendError(HttpStatus.BAD_REQUEST.value(), "Oops, something went wrong");
     }
 }
